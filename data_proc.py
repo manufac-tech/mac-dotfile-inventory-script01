e3720e290a7245f670217120ca2883d30fc4d23b
Steven Brown <1176501+manufactured@users.noreply.github.com>
2024-08-04 12:55:04 -0700

    still broken

--- data_proc.py
+++ data_proc.py
@@ -6,7 +6,6 @@ def create_dataframe(dot_items, template_dict):
     """
     # Prepare the data
     data = {
-        'unique_id': [],  # Adding a unique identifier for each entry
         'line_number': [],
         'line_content': [],
         'fs_filename': [],
@@ -35,7 +34,6 @@ def create_dataframe(dot_items, template_dict):
             fs_type = 'Folder' if dot_items[item_name] else 'File'
 
         # Append the data
-        data['unique_id'].append(line_number)  # Using line_number as a unique ID for simplicity
         data['line_number'].append(line_number)
         data['line_content'].append(line_content)
         data['fs_filename'].append(fs_name)
@@ -48,10 +46,10 @@ def create_dataframe(dot_items, template_dict):
     # Create the DataFrame
     df = pd.DataFrame(data)
 
-    # Ensuring the index is unique by creating a new unique ID
-    df.reset_index(drop=True, inplace=True)
+    # Reset the index to generate unique IDs
+    df.reset_index(inplace=True)
 
-    # Optionally, you can set the unique_id column as the index
-    df.set_index('unique_id', inplace=True)
+    # Rename the default index column to 'unique_id'
+    df.rename(columns={'index': 'unique_id'}, inplace=True)
 
     return df
